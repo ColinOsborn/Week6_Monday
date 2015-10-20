7aeae7d173cf9d918169ff9be5636cbac3e41b5a
Colin Osborn <Colin@Colin-Osborns-MacBook-Pro.local>
2015-10-19 19:27:38 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+$.ajax({
+  url: "https://api.myjson.com/bins/20gec",
+  type: 'GET',
+  data: {
+    format: 'json'
+  },
+  error: function(){
+    alert('An error has occurred');
+  },
+  success: function(results){
+    console.log(results);
+  }
+});
